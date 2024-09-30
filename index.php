d12f0bbb924ade48ac177582e0d0c53efdd2d6c3
micheleboanno92 <michelebonanno92@gmail.com>
2024-10-01 00:57:40 +0200

    stampata tabella con bootstrap

--- index.php
+++ index.php
@@ -49,7 +49,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>PHP HOTEL</title>
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
       
     </head>
     <body>

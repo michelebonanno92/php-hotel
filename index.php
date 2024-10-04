5c548079e94c7f99edab3d44d302888de0cf2351
micheleboanno92 <michelebonanno92@gmail.com>
2024-10-04 14:32:29 +0200

    Bonus 2 aggiunto secondo filtro per il voto

--- index.php
+++ index.php
@@ -39,8 +39,13 @@
         ],
 
     ];
-
-    
+        /*  oppure invece di usare isset si può utiizzare la array_key_exists
+            if (
+                array_key-exists('parking', $_GET) === true 
+                &&
+                $_GET['parking'] !== null
+            )
+        */
         ?>
            
 <!DOCTYPE html>
@@ -54,7 +59,7 @@
       
     </head>
     <body>
-
+    <div class="container">
         <header>
             <h3>
                 PHP HOTEL 
@@ -62,26 +67,49 @@
             <div>
             <form  method="GET">
 
-                <div>
+                <div class="mb-2">
                     <label class="d-block">
                         Parcheggio ?
                     </label>
 
                     <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="1" id="
-                        parking-yes" name="parking">
+                        <input 
+                            class="form-check-input"
+                            type="checkbox" 
+                            value="1" 
+                            id="parking-yes" 
+                            name="parking"
+                            <?php echo (isset($_GET['parking']) && $_GET['parking'] == 1 ? 'checked' : ''); ?> >
+                            <!-- invece di usare il checked abbiamo usato due condizioni -->
                         <label class="form-check-label" for="
                         parking-yes">
                             Si
                         </label>
                     </div>
-                </div>
-                    <button class="btn btn-primary" type="submit">
-                        Invia
-                    </button>
-                <div>
 
                 </div>
+
+                <div class="mb-3 w-25">
+                    <label for="vote" class="form-label">Voto (>=)</label>
+                    <input
+                        type="number" 
+                        class="form-control" 
+                        id="vote" 
+                        min="1"
+                        max="5"
+                        name="vote"
+                        value="<?php echo (isset($_GET['vote']) ? $_GET['vote'] : ''); ?> " >
+                   
+                        
+                </div>
+
+                <div>
+                     <button class="btn btn-primary" type="submit">
+                        Invia
+                    </button>
+                </div>
+                    
+               
                     
             </form>
             </div>
@@ -108,7 +136,7 @@
                                     // isset($var) controlla se la variabile passata come argomento esiste ( quindi è stata definita) e ha un valore diverso da null . Restituisce true/false
 
                                         if ( 
-                                            isset($_GET['parking']) // controllo se c'è un filtraggio per parcheggio attivo 
+                                            isset($_GET['parking']) // controllo se c'è un filtraggio per parcheggio attivo ( cioè controllo se nella query string c'è parking)
                                              && 
                                              $_GET['parking'] == 1  // controllo se il valore del filtro è 1 
                                              && 
@@ -117,18 +145,30 @@
                                             $showHotel = false;
                                         }
 
+                                        if ( 
+                                            isset($_GET['vote']) 
+                                             && 
+                                             is_numeric($_GET['vote'])   // controlla se il valore di vote è un numero
+                                             && 
+                                             $hotel["vote"]  < $_GET['vote'] 
+                                        ) {
+                                            $showHotel = false;
+                                        }
+
+
+
 
                                         if ($showHotel){
-                                            ?>
-                                            <tr>
-                                                <th scope="row"><?php echo $ind + 1; ?> </th>
-                                                <td><?php echo $hotel["name"]; ?></td>
-                                                <td><?php echo $hotel["description"]; ?></td>
-                                                <td><?php echo ($hotel["parking"] == true ?  'Si' : 'No'); ?> </td>
-                                                <td><?php echo $hotel["vote"]; ?></td>
-                                                <td><?php echo $hotel["distance_to_center"]; ?></td>
-                                            </tr>
-                                            <?php
+                                             ?>
+                                        <tr>
+                                            <th scope="row"><?php echo $ind + 1; ?> </th>
+                                            <td><?php echo $hotel["name"]; ?></td>
+                                            <td><?php echo $hotel["description"]; ?></td>
+                                            <td><?php echo ($hotel["parking"] == true ?  'Si' : 'No'); ?> </td>
+                                            <td><?php echo $hotel["vote"]; ?></td>
+                                            <td><?php echo $hotel["distance_to_center"]; ?></td>
+                                        </tr>
+                                        <?php
                                         }
                                    
                         
@@ -138,6 +178,8 @@
                 </table>
             </div>
         </main>
+    </div>
+       
                
     </body>
 </html>

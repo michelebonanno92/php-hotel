5e1f05ec080fbed1faa4a034c3b9e4506af619c5
micheleboanno92 <michelebonanno92@gmail.com>
2024-10-04 12:21:13 +0200

    aggiunto form con un acheckbox e un buttton

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # template-html-css
+<!-- 
+Bonus:
+1 - Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di filtrare gli hotel che hanno un parcheggio.
+2 - Aggiungere un secondo campo al form che permetta di filtrare gli hotel per voto (es. inserisco 3 ed ottengo tutti gli hotel che hanno un voto di tre stelle o superiore)
+NOTA: deve essere possibile utilizzare entrambi i filtri contemporaneamente (es. ottenere una lista con hotel che dispongono di parcheggio e che hanno un voto di tre stelle o superiore)
+Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gli hotel.
+ -->
--- index.php
+++ index.php
@@ -49,56 +49,91 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>PHP HOTEL</title>
+        <!-- link Bootstrap -->
     	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
       
     </head>
     <body>
-            <table class="table">
-                <thead>
-                    <tr>
-                    <th scope="col">Id</th>
-                    <th scope="col">Nome</th>
-                    <th scope="col">Descrizione</th>
-                    <th scope="col">Parcheggio</th>
-                    <th scope="col">Voto</th>
-                    <th scope="col">Distanza dal centro</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                        //  for ( $i = 0 ; $i < count($hotels); $i++) {
-                        foreach ( $hotels as $ind => $hotel) {
-                            ?>
-                                <tr>
-                                    <th scope="row"><?php echo $ind ; ?> </th>
-                                    <td><?php echo $hotel["name"]; ?></td>
-                                    <td><?php echo $hotel["description"]; ?></td>
-                                    <td><?php echo ($hotel["parking"] == true ?  'Si' : 'No'); ?> </td>
-                                    <td><?php echo $hotel["vote"]; ?></td>
-                                    <td><?php echo $hotel["distance_to_center"]; ?></td>
-                                </tr>
-                                <?php
-                   
-                            }
-                         ?>
-                </tbody>
-            </table>
 
-            
-        
+        <header>
+            <h3>
+                PHP HOTEL 
+            </h3>
+            <div>
+            <form  method="GET">
+
+                <div>
+                    <label class="d-block">
+                        Parcheggio ?
+                    </label>
+
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="1" id="
+                        parking-yes" name="parking">
+                        <label class="form-check-label" for="
+                        parking-yes">
+                            Si
+                        </label>
+                    </div>
+                </div>
+                    <button class="btn btn-primary" type="submit">
+                        Invia
+                    </button>
+                <div>
+
+                </div>
+                    
+            </form>
+            </div>
+        </header>
+        <main>
+            <div>
+                <table class="table">
+                        <thead>
+                            <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Descrizione</th>
+                            <th scope="col">Parcheggio</th>
+                            <th scope="col">Voto</th>
+                            <th scope="col">Distanza dal centro</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                                //  for ( $i = 0 ; $i < count($hotels); $i++) {
+                                foreach ( $hotels as $ind => $hotel) {
+                                    $showHotel = false;
+
+                                    // isset($var) controlla se la variabile passata come argomento esiste ( quindi è stata definita) e ha un valore diverso da null . Restituisce true/false
+
+                                        if ( 
+                                            isset($_GET['parking']) // controllo se c'è un filtraggio per parcheggio attivo 
+                                             && 
+                                             $_GET['parking'] == 1  // controllo se il valore del filtro è 1 
+                                             && 
+                                             $hotel["parking"] == false // controllo se l'hotel NON ha il parcheggio 
+                                        ) {
+                                            $showHotel = true;
+                                        }
+                                    ?>
+                                        <tr>
+                                            <th scope="row"><?php echo $ind + 1; ?> </th>
+                                            <td><?php echo $hotel["name"]; ?></td>
+                                            <td><?php echo $hotel["description"]; ?></td>
+                                            <td><?php echo ($hotel["parking"] == true ?  'Si' : 'No'); ?> </td>
+                                            <td><?php echo $hotel["vote"]; ?></td>
+                                            <td><?php echo $hotel["distance_to_center"]; ?></td>
+                                        </tr>
+                                        <?php
+                        
+                                    }
+                                ?>
+                        </tbody>
+                </table>
+            </div>
+        </main>
+               
     </body>
 </html>
 
-<!-- 
-Descrizione
-Partiamo da questo array di hotel. https://www.codepile.net/pile/OEWY7Q1G
-Stampare tutti i nostri hotel con tutti i dati disponibili.
-Iniziate in modo graduale.
-Prima stampate in pagina i dati, senza preoccuparvi dello stile.
-Dopo aggiungete Bootstrap e mostrate le informazioni con una tabella.
-Bonus:
-1 - Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di filtrare gli hotel che hanno un parcheggio.
-2 - Aggiungere un secondo campo al form che permetta di filtrare gli hotel per voto (es. inserisco 3 ed ottengo tutti gli hotel che hanno un voto di tre stelle o superiore)
-NOTA: deve essere possibile utilizzare entrambi i filtri contemporaneamente (es. ottenere una lista con hotel che dispongono di parcheggio e che hanno un voto di tre stelle o superiore)
-Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gli hotel.
- -->
